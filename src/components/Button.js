45ff7be606f2863bc67c810b7eb3163bcd29becf
yangho1231 <yangho1231@gmail.com>
2021-01-21 22:10:07 -0700

    Added new files with button and css

---
+++ src/components/Button.js
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types'
+
+const Button = ({ color, text }) => {
+    return(
+        <button className='btn' style={{ backgroundColor: color}}>{text}</button>
+    )
+}
+Button.defaultProps = {
+    color: 'steelBlue'
+}
+Button.propTypes = {
+    text: PropTypes.string,
+    color: PropTypes.string
+}
+export default Button
---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types'
+import Button from './Button'
+
+const Header = ({ title }) => {
+    return(
+        <header className='header'>
+            <h1>{title}</h1>
+            <Button color='green' text='Add'/>
+        </header>
+            
+    )
+}
+Header.defaultProps = {
+    title: 'Task Tracker',
+}
+
+Header.propTypes = {
+    title: PropTypes.string,
+}
+
+// const headingStyle = {
+//     color: 'red',
+//     backgroundColor: 'black'
+// }
+export default Header
